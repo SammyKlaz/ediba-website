eb3a5f263c0091348818eff435ab431dcf489540
Samuel <samueledet717@gmail.com>
2026-07-01 01:12:45 +0100

    Fixed email.js

--- config/email.js
+++ config/email.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
+  port: 587,
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
@@ -13,4 +13,7 @@ const transporter = nodemailer.createTransport({
   socketTimeout: 10000
 });
 
+console.log("Testing SMTP...");
+await transporter.verify();
+console.log("SMTP connected");
 export default transporter;
